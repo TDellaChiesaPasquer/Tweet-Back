5ec3a7c251cb85ab9c4176097793f42b0033942d
Tim Della-Chiesa Pasquer <tim.dellachiesapasquer@gmail.com>
2025-07-17 10:41:02 +0200

    ajout ahtentification

---
+++ modules/jwt.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+
+//The functions for JWT identification
+
+function generateAccessToken(userId) {        //Generates a token for 24h, stores the username of the user
+    return jwt.sign({userId}, process.env.SECRET_KEY, {expiresIn: '24h'}) 
+}
+
+function authenticateToken(req, res, next) {        //Identify the token, and puts the username in the request
+    const token = req.headers['authorization'];
+    if (token == null) {
+        return res.json({result: false, error: 'Please login.'});   //No custom status to not have an error in the client console
+    }
+    jwt.verify(token, process.env.SECRET_KEY, (err, decoded)=> {
+        if (err) {
+            return res.json({result: false, error: 'Your session is invalid. Please login again.'});
+        }
+        req.userId = new mongoose.Types.ObjectId(decoded.userId);
+        next();
+    })
+}
+
+module.exports = {generateAccessToken, authenticateToken};
