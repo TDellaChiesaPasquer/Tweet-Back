2c0d22d63aab45ba2523927875fd32694b2515ab
Tim Della-Chiesa Pasquer <tim.dellachiesapasquer@gmail.com>
2025-07-17 22:30:40 +0200

    correction route hashtag

--- routes/hashtag.js
+++ routes/hashtag.js
@@ -13,11 +13,17 @@ router.get('/:title',
         if (!errors.isEmpty()) {
             return res.status(400).json({ result: false, error: errors.array() });
         }
-        const possibleHashtag = await Hashtag.findOne({title: {$regex: new RegExp(req.params.title, 'i')}}).populate('tweetList').populate({path:'tweetList', populate: {path:'creator'}});
-        if (!possibleHashtag) {
+        const possibleHashtag = await Hashtag.find({title: {$regex: new RegExp(`#${req.params.title}`, 'i')}}).populate('tweetList').populate({path:'tweetList', populate: {path:'creator'}});
+        if (possibleHashtag.length === 0) {
             return res.json({result: true, tweetList: []})
         }
-        res.json({result: true, tweetList: possibleHashtag.tweetList});
+        const tweetList = [];
+        for (const element of possibleHashtag) {
+            for (const tweet of element.tweetList) {
+                tweetList.push(tweet);
+            }
+        }
+        res.json({result: true, tweetList: [...new Set(tweetList)]});
     } catch(error) {
         console.log(error);
         res.json({result: false, error: 'Server error'})
